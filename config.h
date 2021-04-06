9991c20e120af687fd1248aa86d1db579c2b6db1
prempaolo <prempaolo@gmail.com>
2021-04-06 11:38:49 +0200

    changes

--- config.h
+++ config.h
@@ -77,7 +77,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -86,7 +87,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title						tags mask     iscentered		isfloating   isterminal		noswallow		monitor */
 	{ "Gimp",     NULL,       NULL,       		0,            0,						1,           0,						0,					-1 },
-	{ "firefox",  NULL,       NULL,       		0,						1,						1,           0,						-1,					-1 },
 	{ "Telegram", NULL,       NULL,       		0,						1,						1,           0,						0,					-1 },
 	{ "St",				NULL,				NULL,						0,            1,						0,           1,						-1,					-1 },
 	{ "St",				"float",		NULL,						0,            1,						1,           1,						-1,					-1 },
@@ -109,6 +109,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+//#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
